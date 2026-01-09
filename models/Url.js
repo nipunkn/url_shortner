05aace5820b1d8f622adcb4c8507f6f5afdb0607
NIPUN <157487951+nipunkn@users.noreply.github.com>
2026-01-10 00:09:38 +0530

    Add files via upload

---
+++ models/Url.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema(
+  {
+    originalUrl: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    shortCode: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true
+    },
+
+    clicks: {
+      type: Number,
+      default: 0
+    }
+  },
+  {
+    timestamps: true // adds createdAt & updatedAt automatically
+  }
+);
+
+module.exports = mongoose.model("Url", urlSchema);
